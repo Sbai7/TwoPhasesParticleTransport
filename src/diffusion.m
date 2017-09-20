c3e5e04f3b1a7743efc8b03e3351e89bdc4c8656
Adil Sbai <Sbai7@users.noreply.github.com>
2017-09-20 15:17:36 +0200

    Delete diffusion.m

--- src/diffusion.m
+++
@@ -1,7 +0,0 @@
-function [C,Flux]=diffusion(Grid,particle,C,dt)
-
-% Isotropic diffusion tensor 
-D = particle.diff_coeff.*ones(3,Grid.Nx,Grid.Ny,Grid.Nz);
-
-% Compute particle concentration and extract diffusive fluxes
-[C,Flux] = fv_diffusion(Grid,D,C,dt);
