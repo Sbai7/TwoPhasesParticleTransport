290d33f625a7a11da0c165d1118231551e994b0a
Adil Sbai <Sbai7@users.noreply.github.com>
2017-09-20 15:16:41 +0200

    Delete Rate_d.m

--- src/Rate_d.m
+++
@@ -1,9 +0,0 @@
-function ydot = Rate_d(t, y, ...
-                         C, ...
-                         ah, acl, ad, ... 
-                         net_u_norm, net_conc, u_norm)
-
-ydot    = zeros(length(y),1);
-ydot    = - ah*y.*net_u_norm ...    % Hydrodynamic rel. rate
-          - acl*y.*net_conc  ...    % Colloidal rel. rate 
-          + ad*u_norm.*C;           % Surface dep. rate
